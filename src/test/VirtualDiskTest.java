29e362e8cccf748944f2111c91291349d40078bd
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2014-03-30 19:30:00 +0200

    Added tests for deletion of disk

--- src/test/VirtualDiskTest.java
+++ src/test/VirtualDiskTest.java
@@ -1,16 +1,23 @@
 package test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
 import org.junit.Test;
 
 import virtualDisk.VirtualDisk;
 import exceptions.IncorrectPathException;
 
 public class VirtualDiskTest {
+	private final String WINDOWS_PATH = "D:/test.vdisk";
 
 	@Test
 	public void settingUpDiskWithValidPathShouldWork() throws Exception {
-		VirtualDisk virtualDisk = new VirtualDisk("D:/hello.vdisk");
+		VirtualDisk virtualDisk = new VirtualDisk(WINDOWS_PATH);
 		virtualDisk.setupDisk();
+		virtualDisk.deleteDisk();
 	}
 
 	@Test(expected = IncorrectPathException.class)
@@ -18,5 +25,15 @@ public class VirtualDiskTest {
 			throws Exception {
 		VirtualDisk virtualDisk = new VirtualDisk("D:/");
 		virtualDisk.setupDisk();
+	}
+
+	@Test
+	public void deleteDiskShouldDeleteTheDiskCreated() throws Exception {
+		VirtualDisk virtualDisk = new VirtualDisk(WINDOWS_PATH);
+		virtualDisk.setupDisk();
+		File f = new File(WINDOWS_PATH);
+		assertTrue(f.exists());
+		virtualDisk.deleteDisk();
+		assertFalse(f.exists());
 	}
 }
