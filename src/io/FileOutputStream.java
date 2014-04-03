f460cab0eebebe52165ac6223f4880f1c35c1cb6
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2014-04-03 18:59:07 +0200

    Added stubs for FileOutputStream

---
+++ src/io/FileOutputStream.java
@@ -0,0 +1,22 @@
+package io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * This class implements the OutputStream interface. It abstracts away the lower
+ * level details of writing to the file block allowing us to easily extend the
+ * system
+ * 
+ * @author ROHIT
+ * 
+ */
+public class FileOutputStream extends OutputStream {
+
+	@Override
+	public void write(int b) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
