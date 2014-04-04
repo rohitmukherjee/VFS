31518b4d630f174f4bc54fa047799d0e803af6f0
Jeremy Bradford <jerbrad6@gmail.com>
2014-04-04 15:06:08 +0200

    worked on fixing and testing delete

--- src/test/TestUtilities.java
+++ src/test/TestUtilities.java
@@ -1,18 +1,28 @@
 package test;
 
+import virtualDisk.BlockSettings;
+
 public class TestUtilities {
 
-	public final static String WINDOWS_PATH = "D:/test.vdisk";
+	public final static String WINDOWS_PATH = "/Users/Jeremy/Documents/JCD/test.vdisk";
 	public final static String POSIX_PATH = "~/home";
 	public static final byte[] testBlockDataLessThan = { 1, 0, 1, 0, 1, 0, 1,
 			0, 1, 0 };
 	public static final byte[] testBlockDataFullBlock = { 0, 1, 0, 1, 0, 1, 0,
 			1, 0, 1, 0, 1, 0, 1, 0, 1 };
+	
+	public static final byte[] testBlockData2Blocks = new byte[(int) BlockSettings.DATA_LENGTH/4];
 
 	public static byte[] bigData = new byte[1000];
 
 	public static void bigDataSetup() {
 		for (int i = 0; i < 1000; i++) {
+			bigData[i] = (byte) (i % 2);
+		}
+	} 
+	
+	public static void twoBlockSetup() {
+		for (int i = 0; i < BlockSettings.DATA_LENGTH; ++i) {
 			bigData[i] = (byte) (i % 2);
 		}
 	}
