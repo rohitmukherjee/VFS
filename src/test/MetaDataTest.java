179853b38561a0f2611cdc40bd0b31786526fae6
Jeremy Bradford <jerbrad6@gmail.com>
2014-04-15 19:14:10 +0200

    working on metaData tests

--- src/test/MetaDataTest.java
+++ src/test/MetaDataTest.java
@@ -1,5 +1,10 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -7,13 +12,23 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import utils.BlockSettings;
+import fileManager.MetaData;
+import fileManager.MetaDataUtilities;
+
 public class MetaDataTest {
 	
 	static Logger logger;
+	private static MetaData rootMeta;
+	private static byte[] rootBytes;
 
 	@BeforeClass
 	public static void setUpBeforeClass() {
 		logger = Logger.getLogger(MetaDataTest.class);
+		rootMeta = new MetaData(
+				BlockSettings.ROOT_NAME, BlockSettings.ROOT_PARENT,
+				BlockSettings.ROOT_TYPE, BlockSettings.ROOT_TIMESTAMP);
+		rootBytes = MetaDataUtilities.metaDataToBytes(rootMeta);
 	}
 	
 	@AfterClass
@@ -22,19 +37,34 @@ public class MetaDataTest {
 	}
 	
 	@Before
-	public static void setUp() {
+	public void setUp() {
 		
 	}
 	
 	@After
-	public static void tearDown() {
+	public void tearDown() {
 		
 	}
 	
 	
 	@Test
-	public void testMetaDataByteArrayCtor() {
+	public void testMetaDataByteArrayCtor() throws IOException {
+		MetaData meta = new MetaData(rootBytes);
+		assertEquals(meta.getBlockNumber(), rootMeta.getBlockNumber());
+		assertArrayEquals(meta.getBytes(), rootMeta.getBytes());
+		assertEquals(meta.getName(), rootMeta.getName());
+		assertEquals(meta.getParent(), rootMeta.getParent());
+		assertEquals(meta.getTimestamp(), rootMeta.getTimestamp());
+		assertEquals(meta.getType(), rootMeta.getType());
 		
+		byte[] randomBytes = new byte[utils.BlockSettings.METADATA_LENGTH];
+		for (int i = 0; i < randomBytes.length; ++i) {
+			//put some random stuff in the byte array that still can be interpreted correctly
+			randomBytes[i] = (byte) (i%8 +'0');
+		}
+		MetaData meta2 = new MetaData(randomBytes);
+		byte[] meta2Bytes = meta2.getBytes();
+		assertArrayEquals(meta2Bytes, randomBytes);
 	}
 	
 	@Test
